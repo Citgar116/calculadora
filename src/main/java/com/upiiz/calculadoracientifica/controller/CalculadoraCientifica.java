e665f2dc487f6e9339e994af713a962e9dda3630
Citgar116 <citlaligarcia723@gmail.com>
2026-05-13 19:17:32 -0600

    Cambio en la ruta

--- src/main/java/com/upiiz/calculadoracientifica/controller/CalculadoraCientifica.java
+++ src/main/java/com/upiiz/calculadoracientifica/controller/CalculadoraCientifica.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CalculadoraCientifica {
 
-    @GetMapping("/cientifica")
+    @GetMapping("/")
     public String cientifica(){
         return "cientifica";
     }
